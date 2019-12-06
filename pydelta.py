48b402197b59c567cbfc0357a058558cccf8b1d3
Christian Arnault <arnault@lal.in2p3.fr>
2019-12-06 10:57:26 +0100

    pydelta.py> add stepper class

--- pydelta.py
+++ pydelta.py
@@ -12,9 +12,42 @@ rows = 1000
 dest = "/user/chris.arnault/xyz"
 batch_size = 1000
 
+import time
+
+class Stepper(object):
+    previous_time = None
+
+    def __init__(self):
+        self.previous_time = time.time()
+
+    def show_step(self, label='Initial time'):
+        now = time.time()
+        delta = now - self.previous_time
+
+        if delta < 60:
+            t = '0h0m{:.3f}s'.format(delta)
+        elif delta < 3600:
+            m = int(delta / 60)
+            s = delta - (m*60)
+            t = '0h{}m{:.3f}s'.format(m, s)
+        else:
+            h = int(delta / 3600)
+            d = delta - (h*3600)
+            m = int(d / 60)
+            s = d - (m*60)
+            t = '{}h{}h{:.3f}s'.format(h, m, s)
+
+        print('--------------------------------', label, t)
+
+        self.previous_time = now
+
+        return delta
+
 if __name__ == "__main__":
+    s = Stepper()
     spark = SparkSession.builder.appName("Delta").getOrCreate()
     spark.sparkContext.setLogLevel("ERROR")
+    s.show_step("init")
 
     for i, arg in enumerate(sys.argv[1:]):
         a = arg.split("=")
